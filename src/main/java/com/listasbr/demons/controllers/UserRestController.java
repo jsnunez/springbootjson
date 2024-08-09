43a1ab1a80b247d633bf3b6394f1424e7e9bd012
joan sebastian nuñez serrano <jsnunez94@gmail.com>
2024-08-08 21:50:11 -0500

    carga json

--- src/main/java/com/listasbr/demons/controllers/UserRestController.java
+++ src/main/java/com/listasbr/demons/controllers/UserRestController.java
@@ -1,5 +1,7 @@
 package com.listasbr.demons.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.listasbr.demons.model.User;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,7 +15,31 @@ import java.util.Set;
 public class UserRestController {
 
     private Hashtable<String, User> usuarios = new Hashtable<>();
+  public UserRestController() {
+        loadUsersFromFile();
+    }
 
+    private void loadUsersFromFile() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            File file = new File("usuarios.json");
+            if (file.exists()) {
+                Map<String, User> loadedUsers = objectMapper.readValue(file, new TypeReference<Map<String, User>>() {});
+                usuarios.putAll(loadedUsers);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveUsersToFile() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File("usuarios.json"), usuarios);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     @PostMapping("/crearusuario")
     public User crearusuario(@RequestBody User user) {
         usuarios.put(user.getCedula(), user);
@@ -32,22 +58,22 @@ public class UserRestController {
         return usuarios;
     }
 
-    private void saveUsersToFile() {
-        try (PrintWriter writer = new PrintWriter(new FileWriter("usuarios.json"))) {
-            writer.println("{");
-            Set<Map.Entry<String, User>> entrySet = usuarios.entrySet();
-            for (Map.Entry<String, User> entry : entrySet) {
-                User user = entry.getValue();
-                writer.println("  \"" + entry.getKey() + "\": {");
-                writer.println("    \"nombre\": \"" + user.getNombre() + "\",");
-                writer.println("    \"apellido\": \"" + user.getApellido() + "\",");
-                writer.println("    \"edad\": \"" + user.getEdad() + "\",");
-                writer.println("    \"cedula\": \"" + user.getCedula() + "\"");
-                writer.println("  },");
-            }
-            writer.println("}");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    // private void saveUsersToFile() {
+    //     try (PrintWriter writer = new PrintWriter(new FileWriter("usuarios.json"))) {
+    //         writer.println("{");
+    //         Set<Map.Entry<String, User>> entrySet = usuarios.entrySet();
+    //         for (Map.Entry<String, User> entry : entrySet) {
+    //             User user = entry.getValue();
+    //             writer.println("  \"" + entry.getKey() + "\": {");
+    //             writer.println("    \"nombre\": \"" + user.getNombre() + "\",");
+    //             writer.println("    \"apellido\": \"" + user.getApellido() + "\",");
+    //             writer.println("    \"edad\": \"" + user.getEdad() + "\",");
+    //             writer.println("    \"cedula\": \"" + user.getCedula() + "\"");
+    //             writer.println("  },");
+    //         }
+    //         writer.println("}");
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 }
